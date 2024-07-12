496685a6d32779168ec0682ce8ecc25ae0156728
uzzal2200 <uzzalhasan1238@gmail.com>
2024-07-13 00:31:39 +0600

    fifth project

--- Module - 11/project_4/first_app/urls.py
+++ Module - 11/project_4/first_app/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-
 urlpatterns = [
- 
-    path('',views.index, name = 'home'),
-    path('about/',views.about, name = 'about'),
+    path('', views.home, name = "homepage"),
+    path('about/', views.about, name = "aboutpage"),
+    path('form/', views.submit_form, name = "submit_form"),
+   
 ]
---
+++ Module - 13/fifth_project/first_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+ 
+    path('', views.home, name = 'homepage'),
+    path('about/',views.about, name = 'aboutpage'),
+    path('form/',views.submit_form, name = 'submit_form'),
+    # path('django_form/',views.djangoForm, name = 'django_form'),
+    # path('django_form/',views.StudentForm, name ='django_form'),
+    path('django_form/',views.PasswordValidation, name ='django_form'),
+]
